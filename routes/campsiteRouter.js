68bc5de28bb29c6237cd59ca6a1373171bc4d8cc
Bailey Stritzinger <bailey.stritzinger@gmail.com>
2022-09-09 11:12:53 -0400

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const Campsite = require('../models/campsite');
-
+const authenticate = require('../authenticate');
 const campsiteRouter = express.Router();
+
+campsite.router.use(bodyParser.json());
+
+campsiteRouter.route('/')
+.get((req, res, next) => {
+    Campsite.find()
+    .populate('comments.author')
+    .then(campsites => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsites);
+    })
+    .catch(err => next(err));
+})
+
+campsiteRouter.route('/:campsiteId')
+.get((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
+})
+
 
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite) {
             res.statusCode = 200;
@@ -19,10 +47,11 @@ campsiteRouter.route('/:campsiteId/comments')
     })
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -69,6 +98,7 @@ campsiteRouter.route('/:campsiteId/comments')
 campsiteRouter.route('/:campsiteId/comments/:commentId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
