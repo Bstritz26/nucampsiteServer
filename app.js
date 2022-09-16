773760f9fae86d4ce553ceb2877e60321c03d9b3
Bailey Stritzinger <bailey.stritzinger@gmail.com>
2022-09-16 13:14:33 -0400

    File Upload

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const campsiteRouter = require('./routes/campsiteRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const partnerRouter = require('./routes/partnerRouter');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-
-const url = 'mongodb://localhost:27017/nucampsite';
-const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-});
-
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
-);
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
+const mongoose = require('mongoose');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'),
+    err => console.log(err)
+);
 
 var app = express();
 
@@ -33,35 +33,10 @@ app.all('*', (req, res, next) => {
     if (req.secure) {
       return next();
     } else {
-        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+        console.log(`Redirecting to: https://${req.hostname}: ${app.get('secPort')}${req.url}`);
+        res.redirect(307, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
     }
 });
-
-//app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-function auth(req, res, next) {
-    console.log(req.session);
-
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
-    }
-}
-
-app.use(auth);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -70,14 +45,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -96,3 +76,17 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
