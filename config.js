62a1d7e754da56dce8d0e9ef1a1f821987900a53
Bailey Stritzinger <bailey.stritzinger@gmail.com>
2022-09-17 08:13:34 -0400

    week 4 workshop

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId:'5226947434081476' ,
+        clientSecret: '654c4c0f6231f42bae931557dd0b8bd5'
+    }
 }
